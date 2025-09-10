4ed2c9583719fa3ab34011b99067c24bed323102
Rohit Chauhan <rohitrankmantra12@gmail.com>
2025-09-10 16:05:58 +0530

    done

--- components/Footer.js
+++ components/Footer.js
@@ -96,7 +96,7 @@ export default function Footer() {
                   d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                 />
               </svg>
-              <span>info@tarottimewithvi.com</span>
+              <span>info@tarotwithvi.com</span>
             </motion.div>
           </div>
 
